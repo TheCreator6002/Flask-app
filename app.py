8fa8fac47b968dc55b4ea098762af72a170442b4
Alexandr Kurushin <a.kurushin@pravo.tech>
2021-01-27 22:35:01 +0500

    Refactor app

--- app.py
+++ app.py
@@ -15,13 +15,9 @@ database.session.commit()
 
 from resources.registration import RegistrationAPI
 from resources.authorization import AuthorizationAPI
-from resources.add_consumption import AddConsumption
-from resources.add_income import AddIncome
 
 api.add_resource(RegistrationAPI, '/registration/<login>/<password>/<name>/<last_name>/<email>')
 api.add_resource(AuthorizationAPI, '/authorization/<login>/<password>')
-api.add_resource(AddConsumption, '/add_consumption/<token>')
-api.add_resource(AddIncome, '/add_income/<token>')
 
 
 if __name__ == '__main__':
